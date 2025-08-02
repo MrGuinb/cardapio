958cf616d3670ca930134f4d9e4621629074e447
MrGuinb <94140397+MrGuinb@users.noreply.github.com>
2025-08-02 09:06:30 -0300

    Add files via upload

---
+++ cardapio/config.js
@@ -0,0 +1,11 @@
+// URL of the spreadsheet with menu items (tab: Items)
+const MENU_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSBG-_2QeSW1K1SRESKPfiTlNFKQqADeTe8sTuYOSL3W7VPYvYbJ_wWTg0qv7--1BDYXG2Swd32cTBA/pub?gid=0&single=true&output=csv';
+
+// URL of the spreadsheet with menu categories (tab: Categories)
+const CATEGORIES_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSBG-_2QeSW1K1SRESKPfiTlNFKQqADeTe8sTuYOSL3W7VPYvYbJ_wWTg0qv7--1BDYXG2Swd32cTBA/pub?gid=1781547264&single=true&output=csv';
+
+// URL of the spreadsheet with operating hours (tab: Hours)
+const HOURS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSBG-_2QeSW1K1SRESKPfiTlNFKQqADeTe8sTuYOSL3W7VPYvYbJ_wWTg0qv7--1BDYXG2Swd32cTBA/pub?gid=1048208748&single=true&output=csv';
+
+// URL of the spreadsheet with general settings (tab: Config)
+const CONFIG_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSBG-_2QeSW1K1SRESKPfiTlNFKQqADeTe8sTuYOSL3W7VPYvYbJ_wWTg0qv7--1BDYXG2Swd32cTBA/pub?gid=288593358&single=true&output=csv';
